be50e559cd55cf6da69656453ab8a1a0ec11ea59
viljam ahdekivi <vahdekiv@student.hive.fi>
2025-11-20 15:01:10 +0200

    everything with parsing done except path validation

--- src/map.c
+++ src/map.c
@@ -1,6 +1,8 @@
 
 #include "cub3d.h"
 
+//strlen is -1 because we don't want the newline
+
 void	create_map(char *file)
 {
 	t_map	map;
@@ -8,34 +10,62 @@ void	create_map(char *file)
 
 	map = ft_calloc(sizeof(t_map));
 	if (!map)
-		error_and_exit("Error. Memory allocation failed\n");
+		error_and_exit("Error. Memory allocation failed\n", &map);
 	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		error_and_exit("Error. Invalid file\n", &map);
 	map.line = get_next_line(fd);
-	map.width = ft_strlen(line);
-	if (getmapsize(map, fd))
-		error_and_exit("Error. Map size error\n");
-	create_grid(map);
-	player_pos(map);
-	valid_path(map);
-	validate_elements(map);
+	map.width = ft_strlen(line - 1);
+	map.height = 1;
+	free(map.line);
+	getmapsize(&map, fd);
+	create_grid(&map, file);
+	validate_grid(&map);
+	valid_path(&map);
 }
 
-void	create_grid(t_map *map)
+void	create_grid(t_map *map, char *file)
 {
+	int	fd;
 
-}
-
-void	player_pos(t_map *map)
-{
-
+	map->y = -1;
+	map->grid = malloc(map->height + 1 * sizeof(char *));
+	if (!map->grid)
+		error_and_exit("Error. Malloc failure\n", map);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		error_and_exit("Error. Invalid file\n", map);
+	while (++map->y < map->height)
+	{
+		map->line = get_next_line(fd);
+		map->grid[map->y] = ft_strdup(map->line);
+		if (!map->grid[map->y])
+			error_and_exit("Error. Strdup failure\n", map);
+		map->grid[map->y][ft_strlen(map->grid) - 1] = '\0';
+		free(map->line);
+	}
+	map->grid[map->y] = '\0';
+	close(fd);
 }
 
 void	valid_path(t_map *map)
 {
-
+//copy map
+//flood fill
 }
 
-void	validate_elements(t_map *map)
+void	validate_grid(t_map *map)
 {
-
+	map->y = -1;
+	while (++map->y < map->height)
+	{
+		map->x = -1;
+		while (++map->x < ft_strlen(map.grid[map->y] - 1))
+		{
+			while (map->grid[map->y][map->x] == ' ')
+				map->x++;
+			validate_elements(map);
+		}
+	}
+	count_elements(map);
 }
---
+++ src/map_utils.c
@@ -0,0 +1,59 @@
+
+#include "cub3d.h"
+
+void	getmapsize(t_map *map, int fd)
+{
+	while (1)
+	{
+		map.line = get_next_line(fd);
+		if (!map.line)
+			break ;
+		map.height++;
+		free(map.line);
+	}
+	close(fd);
+}
+
+void	validate_elements(t_map *map)
+{
+	if ((map->y == 0 || map->y == map->height)
+		&& map->grid[map->y][map->x] != '1')
+		error_and_exit("Error. Map not enclosed\n", map);
+	if (map->grid[map->y][map->x] != '1'
+		|| map->grid[map->y][map->x] != '0'
+		|| map->grid[map->y][map->x] != 'N'
+		|| map->grid[map->y][map->x] != 'W'
+		|| map->grid[map->y][map->x] != 'E'
+		|| map->grid[map->y][map->x] != 'S'
+		|| map->grid[map->y][map->x] != ' ')
+		error_and_exit("Error. Invalid elements\n", map);
+	if ((map->grid[map->y][map->x - 1] == ' '
+		&& (map->grid[map->y][map->x] != ' '
+		|| map->grid[map->y][map->x] != '1'))
+		|| map->grid[map->y][ft_strlen(map->grid) - 1] != '1'
+		|| (map->x == 0 && map->grid[map->y][map->x] != ' '
+		&& map->grid[map->y][map->x] != '1'))
+		error_and_exit("Error. Map not enclosed\n", map);
+}
+
+void	count_elements(t_map *map)
+{
+	map->spawncount = 0;
+	map->y = -1;
+	while (++map->y < map->height)
+	{
+		map->x = -1;
+		while (++map->x < ft_strlen(map.grid[map->y] - 1))
+		{
+			if (map->grid[map->y][map->x] == 'N'
+				|| map->grid[map->y][map->x] == 'S'
+				|| map->grid[map->y][map->x] == 'W'
+				|| map->grid[map->y][map->x] == 'E')
+			{
+				map->spawncount++;
+				map->playery = map->y;
+				map->playerx = map->x;
+			}
+		}
+	}
+}
