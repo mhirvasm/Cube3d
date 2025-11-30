accc57219f51f92f82af47bd60ade87f3efd7c11
Miika <mhirvasm@studen.hive.fi>
2025-11-30 11:45:28 +0200

    init_player_vectors should be dooone and ready to roll

--- src/init.c
+++ src/init.c
@@ -14,6 +14,7 @@ int	init_game(t_game *game, char *map_file)
 
 static void	set_player_direction(t_player *player, char dir)
 {
+	//TODO how to make this into 25 lines? HNGGGHGHGHGHG
 	if (dir == 'N')
 	{
 		player->dir.x = 0;
@@ -46,8 +47,18 @@ static void	set_player_direction(t_player *player, char dir)
 
 void	init_player_vectors(t_player *player, t_map *map)
 {
-	//Check the init direction and intialize values. (NSEW)
+	char	dir_char;
+
+	dir_char = map->grid[map->playery][map->playerx]; //saving the NSWE value for setting up playuer dir
+
+	//init and place the player in the middle of the grid by adding 0.5, to evade potential bugs (next to wall example)
+	player->pos.x = (double)map->playerx + 0.5;
+	player->pos.y = (double)map->playery + 0.5; //"playery" lmao xdd
+
+	//Check the init direction and intialize values. (NSEW) 
 	//adjust direction and FOV value
-	//place the player in the middle of the grid by adding 0.5, to evade potential bugs (next to wall example)
+	set_player_direction(&player, dir_char);
+
 	//in the end we want to replace the NSEW value into empty space ('0')!
+	map->grid[map->playery][map->playerx] == '0';
 }
