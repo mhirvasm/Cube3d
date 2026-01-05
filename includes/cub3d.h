83d67d22780c8580d3729d951e43fbc349f40c34
viljam ahdekivi <vahdekiv@student.hive.fi>
2026-01-05 10:53:27 +0200

    .h file norm

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vahdekiv <vahdekiv@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:30:45 by vahdekiv          #+#    #+#             */
-/*   Updated: 2026/01/05 10:30:52 by vahdekiv         ###   ########.fr       */
+/*   Updated: 2026/01/05 10:52:55 by vahdekiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,35 +68,35 @@ typedef struct s_wall_vars
 
 typedef struct s_vector
 {
-    float x;
-    float y;
-} t_vector;
+	float	x;
+	float	y;
+}	t_vector;
 
 typedef struct s_int_point
 {
-    int x;
-    int y;
-} t_point;
+	int		x;
+	int		y;
+}	t_point;
 
 typedef struct s_ray
 {
-    t_vector    direction;  // Direction of the ray
-    t_vector    side_dist;  // distance to next line
-    t_vector    delta_dist; // distance over one square (fixed value)
-    t_point     map;        // current screen
-    t_point     step;       // direction (1 or -1)
-    double      wall_dist;  // final distance
-    int         side;       // (flag) 0 = vertical wall, 1 for horizontal
+	t_vector	direction;// Direction of the ray
+	t_vector	side_dist;// distance to next line
+	t_vector	delta_dist;// distance over one square (fixed value)
+	t_point		map;// current screen
+	t_point		step;// direction (1 or -1)
+	double		wall_dist;// final distance
+	int			side;// (flag) 0 = vertical wall, 1 for horizontal
 	int			line_height;
 	int			draw_start;
 	int			draw_end;
-} t_ray;
+}	t_ray;
 
 typedef struct s_map
 {
 	char		*line;
 	int			height;
-    int			width;
+	int			width;
 	int			y;
 	int			x;
 	int			playery;
@@ -105,74 +105,64 @@ typedef struct s_map
 	char		player_dir;
 	int			spawncount;
 	char		**textures;
-    int         tile_size;
-
+	int			tile_size;
 	int			color;
-
 }	t_map;
 
 typedef struct s_player
 {
-    t_vector    pos;    //precise location
-    t_vector    dir;    // direction vector
-    t_vector    plane;  // cameraplane vector (FOV)
+	t_vector	pos;//precise location
+	t_vector	dir;// direction vector
+	t_vector	plane;// cameraplane vector (FOV)
 	float		angle;
-    bool		key_up;
-    bool		key_down;
-    bool		key_left;
-    bool		key_right;
-    bool		left_rotate;
-    bool		right_rotate;
-}   t_player;
+	bool		key_up;
+	bool		key_down;
+	bool		key_left;
+	bool		key_right;
+	bool		left_rotate;
+	bool		right_rotate;
+}	t_player;
 
 typedef struct s_img
 {
-
-    void	*img;
-    char	*addr;
-    int		bpp;
-    int		size_line;
-    int		endian;
-    int     width;
-    int     height;
-
-} t_img;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		size_line;
+	int		endian;
+	int		width;
+	int		height;
+}	t_img;
 
 typedef struct s_game
 {
-	  t_map	map;
-
-	void	*mlx;
-    void	*win;
-    void    *img;
-
-    char	*data;
-	char	*line;
-    int		bpp;
-    int		size_line;
-    int		endian;
-
-    t_img   walls[4]; //0=North, 1=South, 2=West, 3=East
-	int		color;
-    int     floor_color;
-    int     ceiling_color;
-
-    t_player player;
+	t_map		map;
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*data;
+	char		*line;
+	int			bpp;
+	int			size_line;
+	int			endian;
+	t_img		walls[4];//0=North, 1=South, 2=West, 3=East
+	int			color;
+	int			floor_color;
+	int			ceiling_color;
+	t_player	player;
 }	t_game;
-
-
 
 /* ************************************************************************** */
 /* INITIALIZATION                                                             */
 /* ************************************************************************** */
-int     init_game(t_game *game, char *map_file);
-void    init_player_vectors(t_player *player, t_map *map);
+int		init_game(t_game *game, char *map_file);
+void	init_player_vectors(t_player *player, t_map *map);
 void	textures_init(t_game *game, int fd);
 
 /* ************************************************************************** */
 /* MAP PARSING & READING                                                      */
 /* ************************************************************************** */
-int     check_extension(char *argv);
+int		check_extension(char *argv);
 int		gettextures(t_game *game, int fd);
 void	texture_helper(char **textures, int i, int j, char *line);
 int		texture_validate(t_game *game, int fd);
@@ -182,47 +172,47 @@ int		line_check(char *line, char c, char d);
 int		parse_and_validate_rgb(t_game *game, char *texture);
 int		rgb_helper(t_game *game, char **rgb);
 int		encode_rgb(int r, int g, int b);
-void    create_map(t_game *game, char *map_file);
-void    create_grid(t_game *game, int fd);
+void	create_map(t_game *game, char *map_file);
+void	create_grid(t_game *game, int fd);
 void	grid(t_game *game, t_map *map, int fd);
-void    getmapsize(t_map *map, int fd);
+void	getmapsize(t_map *map, int fd);
 
 /* ************************************************************************** */
 /* MAP VALIDATION                                                             */
 /* ************************************************************************** */
-void    validate_grid(t_game *game);
-void    validate_path(t_game *game);
-void    wall_helper(t_game *game);
+void	validate_grid(t_game *game);
+void	validate_path(t_game *game);
+void	wall_helper(t_game *game);
 void	wall_validation(t_game *game, int x, int y);
-void    validate_elements(t_game *game);
+void	validate_elements(t_game *game);
 void	grid_copy(t_game *game, t_map *copy);
-void    validate_copy(t_game *game, t_map *copy, int x, int y);
-void    count_elements(t_game *game);
-void    flood_fill(t_map *copy, int x, int y);
+void	validate_copy(t_game *game, t_map *copy, int x, int y);
+void	count_elements(t_game *game);
+void	flood_fill(t_map *copy, int x, int y);
 
 /* ************************************************************************** */
 /* RENDERING & GRAPHICS                                                       */
 /* ************************************************************************** */
-int     render_frame(t_game *game);
-void    draw_2d_map(t_game *game);
-void    draw_player(t_game *game, int tile_size);
+int		render_frame(t_game *game);
+void	draw_2d_map(t_game *game);
+void	draw_player(t_game *game, int tile_size);
 void	draw_square(t_game *game, int x, int y, int color);
 void	clear_image(t_game *game);
-void    my_mlx_pixel_put(t_game *game, int x, int y, int color);
-void    init_ray(t_ray *ray, t_player *player, int x);
+void	my_mlx_pixel_put(t_game *game, int x, int y, int color);
+void	init_ray(t_ray *ray, t_player *player, int x);
 void	perform_dda(t_game *game, t_ray *ray);
-void    calculate_wall_dist(t_ray *ray);
-void    draw_walls(t_game *game, int x, t_ray *ray);
-void    raycast(t_game *game);
+void	calculate_wall_dist(t_ray *ray);
+void	draw_walls(t_game *game, int x, t_ray *ray);
+void	raycast(t_game *game);
 void	draw_minimap(t_game *game);
 void	draw_fov(t_game *game, int tile_size);
-int     get_tile_size(t_game *game);
+int		get_tile_size(t_game *game);
 void	draw_player_dir(t_game *game, int tile_size);
 
 /* ************************************************************************** */
 /* INPUT & MOVEMENT                                                           */
 /* ************************************************************************** */
-int     key_press(int keycode, t_game *game);
+int		key_press(int keycode, t_game *game);
 int		key_release(int keycode, t_game *game);
 int		is_wall(t_game *game, int x, int y);
 void	move_player(t_game *game);
@@ -233,9 +223,9 @@ void	move_right_and_left(t_game *game);
 /* ************************************************************************** */
 /* CLEANUP & ERROR HANDLING                                                   */
 /* ************************************************************************** */
-void    free_map(t_map *map);
+void	free_map(t_map *map);
 void	ft_free(char **array);
-void    error_and_exit(char *msg, t_game *game);
-void    error(char *msg);
+void	error_and_exit(char *msg, t_game *game);
+void	error(char *msg);
 
 #endif
